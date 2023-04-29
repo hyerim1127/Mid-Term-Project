f0994b6dbe087d0cc02558fb29a09d18ef6a820d
hyerim <hyerim1148@naver.com>
2023-04-30 03:56:56 +0900

    13th commit

--- mid-term-project-js.js
+++ mid-term-project-js.js
@@ -5,6 +5,7 @@ const top1 = sections[0].offsetTop
 const top2 = sections[1].offsetTop
 const top3 = sections[2].offsetTop
 const top4 = sections[3].offsetTop
+const top5 = sections[4].offsetTop
 
 clicks[0].onclick = function() {
     window.scroll({top:top1, behavior:'smooth'})
@@ -17,4 +18,7 @@ clicks[2].onclick = function() {
 }
 clicks[3].onclick = function() {
     window.scroll({top:top4, behavior:'smooth'})
+}
+clicks[4].onclick = function() {
+    window.scroll({top:top5, behavior:'smooth'})
 }
